e42fad009e8c4eb70bc389159d4a17a9f9f049a0
Oleg <o.antonenko2011@yandex.ru>
2022-11-07 15:09:51 +0300

    Задача 1

---
+++ Задача 1/Program.cs
@@ -0,0 +1,30 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+Console.Write("Введите количество элементов массива: ");
+int ElementsCount = int.Parse(Console.ReadLine());
+    int [] matr  = new int [ElementsCount];
+    for (int i = 0; i < matr.Length; i ++)
+    {
+          Console.Write("Введите число: ");
+          matr [i] = int.Parse(Console.ReadLine());
+    }
+  
+
+int count = 0;
+int Number (int [] matr)
+{
+for (int i = 0; i < matr.Length; i++)
+{
+    if (matr[i] > 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Кол-во элементов больше нуля: {count}");
+return count;
+}
+
+Number(matr);
